24068ef6e6735713257dffa8983fbbb8772dd419
KhachKara <khachkara@gmail.com>
2017-09-17 16:06:16 +0300

    Modified

---
+++ OOP/Multiple_Inheritance.py
@@ -0,0 +1,23 @@
+"""
+Example 4-4: Multiple Inheritance
+"""
+
+
+class A:
+    x = 1
+    y = 'hello'
+
+
+class B:
+    z = 'bye'
+
+
+class C(A, B):
+    def salutation(self):
+        return '%d %s %s' % (self.x, self.y, self.z)
+
+
+inst = C()
+print(inst.salutation())
+inst.x = 100
+print(inst.salutation())
